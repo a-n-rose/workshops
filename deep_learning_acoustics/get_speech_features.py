0de11f8dced2856f918950901ba719d35992d044
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2019-01-18 13:46:46 +0100

    working on functions to extract a variety of helpful features for machine learning, etc.

---
+++ deep_learning_acoustics/get_speech_features.py
@@ -0,0 +1,453 @@
+'''
+ToDo:
+unitests
+
+Speech feature extraction script
+extracts the following features (and then some):
+
+mfcc: mel frequency cepstral coefficients
+meanfreq: mean frequency (in kHz)
+sd: standard deviation of frequency
+median: median frequency (in kHz)
+Q25: first quantile (in kHz)
+Q75: third quantile (in kHz)
+IQR: interquantile range (in kHz)
+skew: skewness
+kurt: kurtosis 
+sp.ent: spectral entropy
+sfm: spectral flatness
+mode: mode frequency
+centroid: frequency centroid
+meanfun: mean fundamental frequency measured across acoustic signal
+minfun: minimum fundamental frequency measured across acoustic signal
+maxfun: maximum fundamental frequency measured across acoustic signal
+meandom: mean of dominant frequency measured across acoustic signal
+mindom: minimum of dominant frequency measured across acoustic signal
+maxdom: maximum of dominant frequency measured across acoustic signal
+dfrange: range of dominant frequency measured across acoustic signal
+modindx: modulation index
+
+'''
+import numpy as np
+import librosa
+import statistics
+from scipy import stats
+
+
+def get_mfcc(y,sr,num_mfcc=None,window_size=None, window_shift=None):
+    '''
+    set values: default for MFCCs extraction:
+    - 40 MFCCs
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if num_mfcc is None:
+        num_mfcc = 40
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    mfccs = librosa.feature.mfcc(y,sr,n_mfcc=num_mfcc,hop_length=hop_length,n_fft=n_fft)
+    mfccs = np.transpose(mfccs)
+    return mfccs
+
+def get_freq_mag(y,sr,window_size=None, window_shift=None):
+    '''
+    default values:
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+    frequencies,magnitudes = librosa.piptrack(y,sr,hop_length=hop_length,n_fft=n_fft)
+    
+    frequencies = np.transpose(frequencies)
+    magnitudes = np.transpose(magnitudes)
+
+    return frequencies, magnitudes
+
+
+def get_freq_mean(y,sr):
+    frequencies, __ = get_freq_mag(y,sr)
+    mean = np.array([np.mean(i) for i in frequencies])
+    return mean
+
+def get_freq_sd(y,sr):
+    frequencies, __ = get_freq_mag(y,sr)
+    std = np.array([np.std(i) for i in frequencies])
+    return std
+
+def get_freq_median(y,sr):
+    '''
+    perhaps better applied to pitch or dominant frequencies?
+    
+    For now just returns 0s
+    '''
+    frequencies, __ = get_freq_mag(y,sr)
+    median = np.array([statistics.median(i) for i in frequencies])
+    return median
+
+def get_freq_mode(y,sr):
+    '''
+    perhaps better applied to pitch or dominant frequencies?
+    
+    For now just returns 0s
+    '''
+    frequencies, __ = get_freq_mag(y,sr)
+    mode = np.array([stats.mode(i)[0] for i in frequencies])
+    return mode
+
+
+def get_fundfreq(y,sr):
+    '''
+    Not 100% sure about this...
+    getting the lowest frequency
+    '''
+    frequencies, __ = get_freq_mag(y,sr)
+    fund_freq = []
+    for i in frequencies:
+        freq = np.unique(i)
+        if freq[0] != 0:
+            print(freq[0])
+            fund_freq.append(freq)
+        else:
+            if len(freq) > 1:
+                fund_freq.append(freq[1])
+    return fund_freq
+
+
+def get_fundfreq_mean(y,sr):
+    fund_freq_mean = np.mean(get_fundfreq(y,sr))
+    return fund_freq_mean
+
+def get_fundfreq_min(y,sr):
+    fund_freq_min = min(get_fundfreq(y,sr))
+    return fund_freq_min
+
+def get_fundfreq_max(y,sr):
+    fund_freq_max = max(get_fundfreq(y,sr))
+    return fund_freq_max
+
+def get_domfreq(y,sr):
+
+    frequencies, magnitudes = get_freq_mag(y,sr)
+    
+    dom_freq_index = [np.argmax(item) for item in magnitudes]
+    
+    dom_freq = [frequencies[i][item] for i,item in enumerate(dom_freq_index) if frequencies[i][item] > 0]
+    return dom_freq
+
+def get_domfreq_mean(y,sr):
+    dom_freq_mean = np.mean(get_domfreq(y,sr))
+    return dom_freq_mean
+
+def get_domfreq_min(y,sr):
+    dom_freq_min = min(get_domfreq(y,sr))
+    return dom_freq_min
+
+def get_domfreq_max(y,sr):
+    dom_freq_max = max(get_domfreq(y,sr))
+    return dom_freq_max
+
+def get_domfreq_range(y,sr):
+    dom_freq_range = get_domfreq_max(y,sr) - get_domfreq_min(y,sr)
+    return dom_freq_range
+
+def get_domfreq_median(y,sr):
+    dom_freq_median = statistics.median(get_domfreq(y,sr))
+    return dom_freq_median
+
+def get_domfreq_mode(y,sr):
+    dom_freq = get_domfreq(y,sr)
+    if len(np.unique(dom_freq))==len(dom_freq):
+        dom_freq = np.array(dom_freq).astype(int)
+    dom_freq_mode = statistics.mode(dom_freq)
+    return dom_freq_mode
+
+def get_1st_3rd_inter_quartile_range(frequency_list):
+    q_1 = np.percentile(frequency_list, 25, interpolation='lower')
+    q_3 = np.percentile(frequency_list, 75, interpolation='higher')
+    q_range = abs(q_1 - q_3)
+    return q_1,q_3,q_range
+
+def get_spectral_skewness():
+    pass
+
+def get_spectral_kurtosis():
+    pass
+
+def get_spectral_entropy():
+    pass
+
+def get_spectral_flatness(y,sr,window_size=None, window_shift=None):
+    '''
+    default values:
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+    flatness = librosa.feature.spectral_flatness(y,sr,n_fft=n_fft,hop_length=hop_length)
+    flatness = np.transpose(flatness)
+    return flatness
+
+def get_spectral_centroid(y,sr,window_size=None, window_shift=None):
+    '''
+    default values:
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+    centroids = librosa.feature.spectral_centroid(y,sr,n_fft=n_fft,hop_length=hop_length)
+    centroids = np.transpose(centroids)
+    return centroids
+
+def get_freq_modulation_index():
+    pass
+
+def get_zero_crossings(y,sr):
+    '''
+    So far only first item in returned list has True value; rest False...
+    '''
+    zero_crossings = librosa.zero_crossings(y,sr)
+    return zero_crossings
+    
+    
+##exprimetnal
+
+def get_cq_transform(y,sr):
+    '''
+    constant-Q transform 
+    default values:
+    - window shifts of 10ms
+    '''   
+    cqt = librosa.cqt(y,sr)
+    cqt = np.transpose(cqt)
+    return cqt
+
+
+def get_chroma_cqt(y,sr):
+    chroma_cqt = librosa.feature.chroma_cqt(y,sr)
+    chroma_cqt = np.transpose(chroma_cqt)
+    return chroma_cqt
+
+def get_chroma_stft(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default for STFT calculation
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    chroma_stft = librosa.feature.chroma_stft(y,sr,hop_length=hop_length,n_fft=n_fft)
+    chroma_stft = np.transpose(chroma_stft)
+    return chroma_stft
+
+
+def get_chroma_cens(y,sr):
+    '''
+    "Chroma Energy Normalized" calculation
+
+    defaults set by Librosa
+    (y=None, sr=22050, C=None, hop_length=512, fmin=None, tuning=None, n_chroma=12, n_octaves=7, bins_per_octave=None, cqt_mode='full', window=None, norm=2, win_len_smooth=41)
+    '''
+    chroma_cens = librosa.feature.chroma_cens(y,sr)
+    chroma_cens = np.transpose(chroma_cens)
+    return chroma_cens
+
+def get_mel_spectrogram(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default for mel spectrogram calculation (FBANK)
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    fbank = librosa.feature.melspectrogram(y,sr,n_fft=n_fft,hop_length=hop_length)
+    fbank = np.transpose(fbank)
+    return fbank
+
+def get_rmse(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default for root-mean-squeare energy
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    rmse = librosa.feature.melspectrogram(y,sr,frame_length=n_fft,hop_length=hop_length)
+    rmse = np.transpose(rmse)
+    return rmse
+
+def get_spectral_bandwidth(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default 
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    bw = librosa.feature.spectral_bandwidth(y,sr,n_fft=n_fft,hop_length=hop_length)
+    bw = np.transpose(bw)
+    return bw
+
+def get_spectral_contrast(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default 
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    contrast = librosa.feature.spectral_contrast(y,sr,n_fft=n_fft,hop_length=hop_length)
+    contrast = np.transpose(contrast)
+    return contrast
+
+def get_spectral_rolloff(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default 
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    rolloff = librosa.feature.spectral_rolloff(y,sr,n_fft=n_fft,hop_length=hop_length)
+    rolloff = np.transpose(rolloff)
+    return rolloff
+
+def get_poly_features(y,sr,window_size=None, window_shift=None):
+    '''
+    set values: default 
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    if window_size is None:
+        n_fft = int(0.025*sr)
+    else:
+        n_fft = int(window_size*0.001*sr)
+    if window_shift is None:
+        hop_length = int(0.010*sr)
+    else:
+        hop_length = int(window_shift*0.001*sr)
+        
+    poly_features = librosa.feature.poly_features(y,sr,n_fft=n_fft,hop_length=hop_length)
+    poly_features = np.transpose(poly_features)
+    return poly_features
+
+def get_tonnetz(y,sr):
+    tonnetz = librosa.feature.tonnetz(y,sr)
+    tonnetz = np.transpose(tonnetz)
+    return tonnetz
+
+def get_zero_crossing_rate(y,window_size=None, window_shift=None):
+    '''
+    set values: default 
+    - windows of 25ms 
+    - window shifts of 10ms
+    '''
+    try:
+        if window_size is None:
+            n_fft = int(0.025*sr)
+        else:
+            n_fft = int(window_size*0.001*sr)
+        if window_shift is None:
+            hop_length = int(0.010*sr)
+        else:
+            hop_length = int(window_shift*0.001*sr)
+            
+        zcr = librosa.feature.zero_crossing_rate(y,frame_length=n_fft,hop_length=hop_length)
+        zcr = np.transpose(zcr)
+        return zcr
+
+    except TypeError as e:
+        print(e)
+        print("Did you include 'sampling rate'? That is not necessary for the function: \n\nget_zero_crossing_rate() \n.")
+
+    return None
+
+def get_notes_from_freq(frequency,octave=True, cents=True):
+    '''
+    input single or list of frequencies
+    
+    output single or list of notes
+    
+    defaults:
+    octave = true --> shows which octave
+    cents = true --> shows fractions
+    NOTE: if cents == True, Octave also has to be True
+    #'''
+    #to prevent error:
+    if cents == True:
+        if octave == False:
+            print("If 'cents=True', 'octave' also has to be true.")
+            octave = True
+    notes = librosa.hz_to_note(frequency,octave=octave,cents=cents)
+    return notes
+
+
---
+++ deep_learning_acoustics/speech_feature_extraction.py
@@ -0,0 +1,71 @@
+'''
+explore feature extraction 
+'''
+
+import librosa
+import get_speech_features as sf
+
+
+def print_shape(matrix_name,matrix):
+    if isinstance(matrix,tuple):
+        matrix = matrix[0]
+    elif isinstance(matrix,list):
+        shape = len(matrix)
+    elif matrix.shape == ():
+        shape = 1
+        print(matrix)
+    else:
+        shape = matrix.shape
+        
+    print("Shape of {}: {}".format(matrix_name,shape))
+
+if __name__=="__main__":
+
+    filename = "speech_modelready_aislyn.wav"
+    sr = 16000
+    y,sr = librosa.load(filename,sr=sr)
+
+    mfccs = sf.get_mfcc(y,sr)
+    
+
+    centroids = sf.get_spectral_centroid(y,sr)
+    
+    freq_mean = sf.get_freq_mean(y,sr)
+    freq_sd = sf.get_freq_sd(y,sr)
+    freq_median = sf.get_freq_median(y,sr)
+    freq_mode = sf.get_freq_mode(y,sr)
+    
+    domfreq_mean = sf.get_domfreq_mean(y,sr)
+    domfreq_min = sf.get_domfreq_min(y,sr)
+    domfreq_max = sf.get_domfreq_max(y,sr)
+    domfreq_range = sf.get_domfreq_range(y,sr)
+    domfreq_median = sf.get_domfreq_median(y,sr)
+    domfreq_mode = sf.get_domfreq_mode(y,sr)
+    domfreq = sf.get_domfreq(y,sr)
+    domfreq_first_quartile, domfreq_third_quartile, domfreq_interquartile_range = sf.get_1st_3rd_inter_quartile_range(domfreq)
+    
+    fundfreq = sf.get_fundfreq(y,sr)
+    fundfreq_mean = sf.get_fundfreq_mean(y,sr)
+    fundfreq_min = sf.get_fundfreq_min(y,sr)
+    fundfreq_max = sf.get_fundfreq_max(y,sr)
+    fundfreq_first_quartile,fundfreq_third_quartile,fundfreq_interquartile_range = sf.get_1st_3rd_inter_quartile_range(fundfreq)
+    
+    fundfreq_notes = sf.get_notes_from_freq(fundfreq)
+    print("\n\nFundamental Frequency Notes:")
+    print(fundfreq_notes)
+    domfreq_notes = sf.get_notes_from_freq(domfreq)
+    print("\n\nDominant Frequency Notes:")
+    print(domfreq_notes)
+    print("\n\n")
+    
+    x = fundfreq
+    print(x)
+    
+    
+
+    features_list = [("MFCCs",mfccs),("Centroids",centroids),("Frequency Mean",freq_mean),("Frequency Standard Deviation",freq_sd),("Frequency Median",freq_median),("Frequency Mode",freq_mode),("Dominant Frequency Mean",domfreq_mean),("Dominant Frequency Minimum",domfreq_min),("Dominant Frequency Maximum",domfreq_max),("Dominant Frequency Range",domfreq_range),("Dominant Frequency Median",domfreq_median),("Dominant Frequency Mode",domfreq_mode),("Dominant Frequency",domfreq),("Dominant Frequency 1st Quartile",domfreq_first_quartile),("Dominant Frequency 3rd Quartile",domfreq_third_quartile),("Dominant Frequency Interquartile Range",domfreq_interquartile_range),("Fundamental Frequency Mean",fundfreq_mean),("Fundamental Frequency Minimum",fundfreq_min),("Fundamental Frequency Maximum",fundfreq_max),("Fundamental Frequency",fundfreq),("Fundamental Frequency 1st Quartile",fundfreq_first_quartile),("Fundamental Frequency 3rd Quartile",fundfreq_third_quartile),("Fundamental Frequency Interquartile Range",fundfreq_interquartile_range)] 
+    
+
+    
+    for item in features_list:
+        print_shape(item[0],item[1])
