1689945b6095908209ae961a59f90bc0d0de4aae
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2019-01-20 21:18:13 +0100

    added more interface options

--- speech_recognition/feature_extraction.py
+++ speech_recognition/feature_extraction.py
@@ -22,16 +22,30 @@ if __name__=="__main__":
         feature_type = "mfcc_pitch"
         num_features = 41
         noise = False
+        label_data_type = "TEXT"
         
         print("Create NEW table? (Y/N)")
         new_table = input()
         if "y" in new_table.lower():
-            feature_type, num_features, noise = user_input.create_new_table(database)
+            tablename, feature_type, num_features, label_column, label_data_type, noise = user_input.create_new_table(database)
         elif "exit" in new_table.lower():
             raise ExitApp()
         
         
         paths, labels = featfun.collect_audio_and_labels()
+        
+        print("Would you like to extract the features and save the data to an SQL table? (Y/N)")
+        cont = input()
+        if "y" in cont.lower() or cont == "":
+            pass
+        else:
+            raise ExitApp()
+        
+        if "tablename" not in locals():
+            print("What is the SQL table to insert this data?")
+            tablename = input()
+            if "exit" in tablename.lower():
+                raise ExitApp()
         
         if noise:
             '''
@@ -43,10 +57,12 @@ if __name__=="__main__":
         for i, wav in enumerate(paths):
             if i <= 10:
                 features = featfun.get_features(wav,feature_type,num_features,noise)
-                dict_data, ex_speaker = featfun.organize_data(dict_data,labels[i],features)
+                dict_data = featfun.organize_data(dict_data,labels[i],features)
                 
         #need to save to sql table
-        
+        data_prepped = featfun.prep_data4sql(dict_data)
+        print(len(data_prepped))
+        user_input.save2sql(database,tablename,data_prepped)
         
     except ExitApp:
         print("Have a good day!")
