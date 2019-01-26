1ae65113f05f290af1947759f70c488aef2750e3
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2019-01-26 23:58:13 +0100

    removed unnecessary comments

--- speech_recognition/train_generator_model.py
+++ speech_recognition/train_generator_model.py
@@ -1,13 +1,8 @@
 '''
 installed Pillow-5.4.1
 
-if I want to include callbacks, explore this: https://github.com/keras-team/keras/issues/6309
-
-problem: accuracy and validation accuracy stay the same after the first couple of epochs
-https://stackoverflow.com/questions/37213388/keras-accuracy-does-not-change
-Perhaps use different optimizer?
-
-need to end training early somehow, with fit_generator()
+1) try out paper implementations
+2) try w ConvLSTM2D
 '''
 
 import time
@@ -44,7 +39,8 @@ frame_width = 19
 num_features = 120
 color_scale = 3 # if 'rgb', color_scale = 3, if 'grayscale', color_scale = 1
 num_labels = 30
-epochs = 3
+epochs = 40
+loss_type = 'sgd'
 if split:
     frame_width_total = frame_width
     shuffle_data = False
@@ -72,7 +68,6 @@ logging.info("SESSION: \n\n{}".format(session_name))
 
 #get total number of images for train, val, and test datasets
 #need this for .fit_generator
-#https://www.pyimagesearch.com/2018/12/24/how-to-use-keras-fit-and-fit_generator-a-hands-on-tutorial/
 num_train_images = featfun.get_num_images('{}_train'.format(data_png))
 num_val_images = featfun.get_num_images('{}_val'.format(data_png))
 num_test_images = featfun.get_num_images('{}_test'.format(data_png))
@@ -98,26 +93,7 @@ model.add(TimeDistributed(tfcnn,input_shape=(timestep,num_features,frame_width,c
 model.add(LSTM(timestep,return_sequences=True,unroll=True)) #num timesteps
 model.add(Flatten())
 model.add(Dense(num_labels,activation="softmax")) # binary = "sigmoid"; multiple classification = "softmax"
-#ValueError: Input arrays should have the same number of samples as target arrays. Found 1 input samples and 5 target samples
-#model.add(TimeDistributed(Dense(num_labels,activation="softmax")))
-#https://github.com/keras-team/keras/issues/4870
 
-
-
-#2) try with ConvLSTM2D
-
-#model = Sequential()
-##samples, time, rows, cols, channels
-#input_shape = (None,120,19,3)
-#model.add(ConvLSTM2D(5,kernel_size=(3,3),activation='relu',padding='valid',input_shape=input_shape))
-
-#model.add(Dense(num_labels,activation='softmax'))
-
-#input_shape = (1,120,19,3)
-##ValueError: Error when checking input: expected conv_lst_m2d_1_input to have 5 dimensions, but got array with shape (1, 120, 19, 3)
-
-#input_shape = (None,1,120,19,3)
-##ValueError: Input 0 is incompatible with layer conv_lst_m2d_1: expected ndim=5, found ndim=6
 
 
 print(model.summary())
@@ -127,7 +103,7 @@ loss = "categorical_crossentropy"
 print("Loss set at: '{}'".format(loss))
 
 #compile model
-model.compile(optimizer='sgd',loss=loss,metrics=['accuracy'])
+model.compile(optimizer=loss_type,loss=loss,metrics=['accuracy'])
 
 
 
@@ -188,8 +164,6 @@ def valGeneratorFunc():
         y = y[0,] #save just one from the 6 from the batchsize - they all have the same label
         y = np.reshape(y,(1,y.shape[0]))
         yield x, y
-#after this fix (keeping only the first y value (the other all have same y value - represent the same utterance)) after the first epoch:
-#ValueError: Error when checking input: expected time_distributed_1_input to have shape (5, 120, 19, 3) but got array with shape (4, 120, 19, 3)
         
         
 trainGenerator = trainGeneratorFunc()
@@ -197,16 +171,6 @@ valGenerator = valGeneratorFunc()
 testGenerator = valGeneratorFunc()
 
 
-#history = model.fit_generator(
-        #trainGenerator,
-        #steps_per_epoch = num_train_images // batch_size,
-        #epochs = 50,
-        #validation_data = valGenerator, 
-        #validation_steps = num_val_images // batch_size
-        #)
-#after this fix.. after the first epoch:
-#ValueError: Error when checking input: expected time_distributed_1_input to have shape (5, 120, 19, 3) but got array with shape (4, 120, 19, 3)
-#https://github.com/keras-team/keras/issues/10164
 
 model_name = "CNN_LSTM_generator_testing_{}epochs_{}trainimages_{}valimages".format(epochs,num_train_images,num_val_images)
 
@@ -217,19 +181,15 @@ csv_logging = CSVLogger(filename='model_log/{}_log.csv'.format(model_name))
 checkpoint_callback = ModelCheckpoint(model_name+'.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 
 
-
-#history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size),
-            #steps_per_epoch=len(X_train) / batch_size, validation_data=(X_test, y_test),
-            #epochs=n_epochs, callbacks=[early_stopping_callback, checkpoint_callback])
-#https://stackoverflow.com/questions/44051402/keras-early-stopping-model-saving
 history = model.fit_generator(
         trainGenerator,
-        steps_per_epoch = num_train_images//batch_size,
+        steps_per_epoch = (num_train_images//batch_size)/4,
         epochs = epochs,
         callbacks=[early_stopping_callback, checkpoint_callback],
         validation_data = valGenerator, 
-        validation_steps = num_val_images//batch_size
+        validation_steps = (num_val_images//batch_size)/4
         )
+
 
 score = model.evaluate_generator(testGenerator, num_test_images//batch_size)
 
@@ -240,10 +200,11 @@ msg="Model Accuracy on test data: {}%\nModel Loss on test data: {}".format(acc,l
 print(msg)
 logging.info(msg)
 
-modelname_final = "CNN_LSTM_{}_{}_{}_{}TrVaTe_images_{}epochs_{}acc".format(session_name,num_train_images,num_val_images,num_test_images,epochs,acc)
+modelname_final = "CNN_LSTM_{}_{}_{}_{}TrVaTe_images_{}epochs_{}Optimizer_{}acc".format(session_name,num_train_images,num_val_images,num_test_images,epochs,loss_type,acc)
 print('Saving Model')
 model.save(modelname_final+'.h5')
 print('Done!')
+logging.info("\n\nModel saved as:\n{}".format(modelname_final))
 print("\n\nModel saved as:\n{}".format(modelname_final))
 
 
