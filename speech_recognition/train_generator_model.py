22bc4ee1218be78e37b709ba7df22a9887ccf62a
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2019-01-28 23:44:31 +0100

    improved generator - fine-tuned how the generator fed data to model
    trains CNN+LSTM but is overfitting;

--- speech_recognition/train_generator_model.py
+++ speech_recognition/train_generator_model.py
@@ -1,6 +1,10 @@
 '''
 installed Pillow-5.4.1
 
+** make sure know how balanced the groups are..
+
+remove beginning and ending silences
+
 1) try out paper implementations
 2) try w ConvLSTM2D
 '''
@@ -30,25 +34,37 @@ logger = logging.getLogger(__name__)
 
 
 #set variables
-modelname = "CNN_LSTM_speech_recognition"
-script_purpose = "Apply_ConvNetLSTM_chromagrams_generator"
-batch_size = 6 #number of 19-framed segments of 120 features (and 3rgb values)
-split = True
+modelname = "CNN_LSTM_speech_commands"
+script_purpose = "speech_commands_CNN_LSTM_ImageDataGenerator"
+batch_size = 1 #number of 19-framed segments of 120 features (and 3rgb values)
+split = False #if True, data won't get shuffled
 timestep = 6
 frame_width = 19
 num_features = 120
 color_scale = 3 # if 'rgb', color_scale = 3, if 'grayscale', color_scale = 1
-num_labels = 30
-epochs = 40
-loss_type = 'sgd'
+num_labels = 3 #if two labels, then put 1
+if num_labels <= 2:
+    loss_type = 'binary_crossentropy'
+    category_type = 'binary'
+    multiple_classification = False
+else:
+    loss_type = 'categorical_crossentropy'
+    category_type = 'categorical'
+    multiple_classification = True
+sparse_targets = False
+if sparse_targets:
+    loss_type = 'sparse_categorical_crossentropy' # if data have mutiple labels which are only integer encoded, *not* one hot encoded.
+epochs = 25
+#loss_type = 'binary_crossentropy'#'sgd', 'categorical_crossentropy', 'sparse_categorical_crossentropy'
+optimizer = 'sgd'# 'adam' 'sgd'
 if split:
     frame_width_total = frame_width
     shuffle_data = False
-    data_png = "data_split"
+    data_png = "data_3word"
 else:
     frame_width_total = frame_width * timestep
-    shuffle_data = True
-    data_png = "data"
+    shuffle_data = False #think it only shuffles inside the picture?
+    data_png = "data_test_nosplit"
 if color_scale == 1:
     color_str = "grayscale"
 elif color_scale == 3:
@@ -65,7 +81,7 @@ logging.info(separator)
 logging.info("RUNNING SCRIPT: \n\n{}".format(current_filename))
 logging.info("SESSION: \n\n{}".format(session_name))
 
-
+logging.info("Pulling data from: {}".format(data_png))
 #get total number of images for train, val, and test datasets
 #need this for .fit_generator
 num_train_images = featfun.get_num_images('{}_train'.format(data_png))
@@ -92,18 +108,18 @@ model = Sequential()
 model.add(TimeDistributed(tfcnn,input_shape=(timestep,num_features,frame_width,color_scale)))
 model.add(LSTM(timestep,return_sequences=True,unroll=True)) #num timesteps
 model.add(Flatten())
-model.add(Dense(num_labels,activation="softmax")) # binary = "sigmoid"; multiple classification = "softmax"
+model.add(Dense(num_labels,activation="sigmoid")) # binary = "sigmoid"; multiple classification = "softmax"
 
 
 
 print(model.summary())
 
 
-loss = "categorical_crossentropy"
-print("Loss set at: '{}'".format(loss))
+#loss = "categorical_crossentropy"
+print("Loss set at: '{}'".format(loss_type))
 
 #compile model
-model.compile(optimizer=loss_type,loss=loss,metrics=['accuracy'])
+model.compile(optimizer=optimizer,loss=loss_type,metrics=['accuracy'])
 
 
 
@@ -123,7 +139,7 @@ train_generator = train_datagen.flow_from_directory(
         target_size=(num_features, frame_width_total),
         batch_size=batch_size,
         color_mode = color_str,       
-        class_mode='categorical',
+        class_mode=category_type,
         shuffle = shuffle_data,
         )
 
@@ -132,7 +148,7 @@ val_generator = test_datagen.flow_from_directory(
         target_size=(num_features, frame_width_total),
         batch_size=batch_size,
         color_mode = color_str,        
-        class_mode='categorical',
+        class_mode=category_type,
         shuffle = shuffle_data,
         )
 
@@ -141,7 +157,7 @@ test_generator = test_datagen.flow_from_directory(
         target_size=(num_features, frame_width_total),
         batch_size=batch_size, #default is 32
         color_mode = color_str,      
-        class_mode='categorical',
+        class_mode=category_type,
         shuffle = shuffle_data,
         )
 
@@ -152,27 +168,77 @@ test_generator = test_datagen.flow_from_directory(
 def trainGeneratorFunc():
     while True:
         x, y = train_generator.next()
-        x = np.reshape(x,(1,x.shape[0],x.shape[1],x.shape[2],color_scale))
-        y = y[0,] #save just one from the 6 from the batchsize - they all have the same label
-        y = np.reshape(y,(1,y.shape[0]))
+        x = np.reshape(x,(x.shape[1],x.shape[2],color_scale))
+        image_sections = []
+        for image_section in range(timestep):
+            if image_section == 0:
+                sect = x[:,image_section:frame_width,:]
+            else:
+                sect = x[:,image_section*frame_width:image_section*frame_width+frame_width,:]
+            image_sections.append(sect)
+        x = np.array(image_sections)
+        x = np.reshape(x, (1,) + x.shape)
+        y = y[0,]#save just one from the 6 from the batchsize - they all have the same label
+        if multiple_classification:
+            y = np.reshape(y,(1,y.shape[0]))
+        
+        else:
+            y = np.reshape(y,(1,)+y.shape)
+            #otherwise, got an error that tuple was out of range (if I remember correctly)
         yield x, y
         
 def valGeneratorFunc():
     while True:
         x, y = val_generator.next()
-        x = np.reshape(x, (1,x.shape[0],x.shape[1],x.shape[2],color_scale))
-        y = y[0,] #save just one from the 6 from the batchsize - they all have the same label
-        y = np.reshape(y,(1,y.shape[0]))
+        x = np.reshape(x,(x.shape[1],x.shape[2],color_scale))
+        image_sections = []
+        for image_section in range(timestep):
+            if image_section == 0:
+                sect = x[:,image_section:frame_width,:]
+            else:
+                sect = x[:,image_section*frame_width:image_section*frame_width+frame_width,:]
+            image_sections.append(sect)
+        x = np.array(image_sections)
+        x = np.reshape(x, (1,) + x.shape)
+        y = y[0,]#save just one from the 6 from the batchsize - they all have the same label
+        if multiple_classification:
+            y = np.reshape(y,(1,y.shape[0]))
+        
+        else:
+            y = np.reshape(y,(1,)+y.shape)
+            #otherwise, got an error that tuple was out of range (if I remember correctly)
+        yield x, y
+        
+def testGeneratorFunc():
+    while True:
+        x, y = test_generator.next()
+        x = np.reshape(x,(x.shape[1],x.shape[2],color_scale))
+        image_sections = []
+        for image_section in range(timestep):
+            if image_section == 0:
+                sect = x[:,image_section:frame_width,:]
+            else:
+                sect = x[:,image_section*frame_width:image_section*frame_width+frame_width,:]
+            image_sections.append(sect)
+        x = np.array(image_sections)
+        x = np.reshape(x, (1,) + x.shape)
+        y = y[0,]#save just one from the 6 from the batchsize - they all have the same label
+        if multiple_classification:
+            y = np.reshape(y,(1,y.shape[0]))
+        
+        else:
+            y = np.reshape(y,(1,)+y.shape)
+            #otherwise, got an error that tuple was out of range (if I remember correctly)
         yield x, y
         
         
 trainGenerator = trainGeneratorFunc()
 valGenerator = valGeneratorFunc()
-testGenerator = valGeneratorFunc()
+testGenerator = testGeneratorFunc()
 
 
 
-model_name = "CNN_LSTM_generator_testing_{}epochs_{}trainimages_{}valimages".format(epochs,num_train_images,num_val_images)
+model_name = "{}_{}epochs_{}trainimages_{}valimages".format(modelname,epochs,num_train_images,num_val_images)
 
 early_stopping_callback = EarlyStopping(monitor='val_loss', patience=5)
 
@@ -183,11 +249,11 @@ checkpoint_callback = ModelCheckpoint(model_name+'.h5', monitor='val_loss', verb
 
 history = model.fit_generator(
         trainGenerator,
-        steps_per_epoch = (num_train_images//batch_size)/4,
+        steps_per_epoch = num_train_images//batch_size,
         epochs = epochs,
         callbacks=[early_stopping_callback, checkpoint_callback],
         validation_data = valGenerator, 
-        validation_steps = (num_val_images//batch_size)/4
+        validation_steps = num_val_images//batch_size
         )
 
 
@@ -200,7 +266,7 @@ msg="Model Accuracy on test data: {}%\nModel Loss on test data: {}".format(acc,l
 print(msg)
 logging.info(msg)
 
-modelname_final = "CNN_LSTM_{}_{}_{}_{}TrVaTe_images_{}epochs_{}Optimizer_{}acc".format(session_name,num_train_images,num_val_images,num_test_images,epochs,loss_type,acc)
+modelname_final = "{}_{}_{}tr_{}va_{}te_images_{}epochs_{}Optimizer_{}acc".format(modelname,session_name,num_train_images,num_val_images,num_test_images,epochs,optimizer,acc)
 print('Saving Model')
 model.save(modelname_final+'.h5')
 print('Done!')
